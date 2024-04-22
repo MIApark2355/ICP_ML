35a644e5c4787597c08d6514716db41647ef3bd0
Mia Park <mia.p@wustl.edu>
2024-04-21 21:08:21 -0500

    testing file : get_bucket.py

--- get_bucket.py
+++ get_bucket.py
@@ -7,27 +7,26 @@ storage_client = storage.Client(project=project_id)
 
 bucket_name = '531-project-team11'
 
-prefix = 'matched/matched/p00/'
+prefix = 'icp_files_24' #filtered dataset folder
 
 bucket = storage_client.get_bucket(bucket_name)
 
-def list_blobs_with_prefix(bucket, prefix):
-    blobs = bucket.list_blobs(prefix=prefix)
-    return [blob for blob in blobs]
+blobs = bucket.list_blobs(prefix=prefix)
 
 # Filter out the files that have ICP in the header
 icp_files = []
 count = 0
-blobs = list_blobs_with_prefix(bucket, prefix)
 for blob in blobs:
     # Read the header file (n.hea) content
-    if blob.name.endswith('n.hea'):
+    if blob.name.endswith('.hea'):
         header_content = blob.download_as_text()
 
         # Check if 'ICP' 
         if 'ICP' in header_content:
-            record_name = blob.name[:-4]
-            print(blob.name)
-            icp_files.append(record_name)
+            count +=1
+            print("icp contained")
+            # record_name = blob.name[:-4]
+            # print(blob.name)
+            # icp_files.append(record_name)
 
-print("Files with ICP in the header:", icp_files)
+print("number of Files with ICP in the header:", count)
